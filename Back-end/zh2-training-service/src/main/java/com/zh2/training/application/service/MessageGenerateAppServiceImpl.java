a74e30b718c517a3be809d5a5507446608d6a7a9
zhaochen <834715766@qq.com>
2020-08-02 15:40:31 +0800

    "报文简电拆分功能"

--- Back-end/zh2-training-service/src/main/java/com/zh2/training/application/service/MessageGenerateAppServiceImpl.java
+++ Back-end/zh2-training-service/src/main/java/com/zh2/training/application/service/MessageGenerateAppServiceImpl.java
@@ -2,10 +2,6 @@ package com.zh2.training.application.service;
 
 import com.zh2.training.domain.message.Message;
 import com.zh2.training.domain.message.MessageHandleService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
