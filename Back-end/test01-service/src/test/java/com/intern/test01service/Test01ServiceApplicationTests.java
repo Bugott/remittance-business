aafc55a19f766b625e30eb0b078e8ff30259a965
Bugott <bugott@foxmail.com>
2020-08-20 16:39:05 +0800

    上传前端相关文件

---
+++ Back-end/test01-service/src/test/java/com/intern/test01service/Test01ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intern.test01service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test01ServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
